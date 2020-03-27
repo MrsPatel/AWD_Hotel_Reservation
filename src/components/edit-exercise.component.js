e602d90eb5f5ad9d58effa16e9a7ab30b61eb052
sriver7 <rsonny89@gmail.com>
2020-03-26 21:15:33 -0400

    Fixed edit functionality

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -1,174 +1,178 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
-export default class EditEcercises extends Component {
-    //all components that have a constructor should have a super props call
-    constructor(props) {
-        super(props);
+export default class EditExercise extends Component {
+  //all components that have a constructor should have a super props call
+  constructor(props) {
+    super(props);
 
-        //Binding 'this' to each method so that 'this' refers to the right thing
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangeDescription = this.onChangeDescription.bind(this);
-        this.onChangeDuration = this.onChangeDuration.bind(this);
-        this.onChangeDate = this.onChangeDate.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+    //Binding 'this' to each method so that 'this' refers to the right thing
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-        this.state = {
-            username : '',
-            description: '',
-            duration: 0,
-            date: new Date(),
-            users: []
-            //Users array so that we can get a dropdown
-        }
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+      //Users array so that we can get a dropdown
     }
+  }
 
-    //React lifecycle method
-    //Called right before anything loads on page
-    //data is an array 
-    //just user.username because that is the only thing we care about
-    componentDidMount() {
-        //getting the id directly from the URL
-        axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
-        //response returns the exercise
-        .then(response => {
+  //React lifecycle method
+  //Called right before anything loads on page
+  //data is an array 
+  //just user.username because that is the only thing we care about
+  componentDidMount() {
+    //getting the id directly from the URL
+    axios.get('http://localhost:5000/exercises/' + this.props.match.params.id)
+      //response returns the exercise
+      .then(response => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          duration: response.data.duration,
+          date: new Date(response.data.date)
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
           this.setState({
-            username: response.data.username,
-            description: response.data.description,
-            duration: response.data.duration,
-            date: new Date(response.data.date)
-          })   
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-
-        axios.get('http://localhost:5000/users/')
-            .then(response => {
-                if (response.data.length > 0) {
-                    this.setState({
-                        users: response.data.map(user => user.username),
-                    })
-                }
-            })
-    }
-
-    //to change the state when username is
-    /*
-    Textbox with username 
-    When someone enter user name it calls function 
-    sets state e.target.value
-    The target is the textbox. 
-    The value of username is whatever is in the textbox 
-
-
-    TODO: Do the same for the other ones 
-    */
-    onChangeUsername(e) {
-        this.setState({
-            username: e.target.value
-        })
-    }
-
-    onChangeDescription(e) {
-        this.setState({
-            description: e.target.value
-        })
-    }
-
-    onChangeDuration(e) {
-        this.setState({
-            duration: e.target.value
-        })
-    }
-
-    //different due to calendar will be clickable
-    onChangeDate(date) {
-        this.setState({
-            date: date
-        })
-    }
-
-    onSubmit(e){
-        e.preventDefault();
-
-        const exercise = {
-            username: this.state.username,
-            description: this.state.description, 
-            duration: this.state.duration, 
-            date: this.state.date
+            users: response.data.map(user => user.username),
+          })
         }
-        //Eventually we'll send to the database
-        console.log(exercise);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
 
-        //expecting a json post
-        //user is the json 
-        //from users.js '/add'
-        //                                                  to pass this id into the url
-        axios.post('http://localhost:5000/exercises/update'+this.props.match.params.id, exercise)
-            .then(res => console.log(res.data));
+  }
+  //to change the state when username is
+  /*
+  Textbox with username 
+  When someone enter user name it calls function 
+  sets state e.target.value
+  The target is the textbox. 
+  The value of username is whatever is in the textbox 
 
-        //Take the person back to the homepage
-        window.location = "/";
+
+  TODO: Do the same for the other ones 
+  */
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    })
+  }
+
+  //different due to calendar will be clickable
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
     }
+    //Eventually we'll send to the database
+    console.log(exercise);
+    //expecting a json post
+    //user is the json 
+    //from users.js '/add'
+    //                                                  to pass this id into the url
+    axios.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
+      .then(res => console.log(res.data));
 
-    render() {
-        return (
-        <div>
-            <h3>Edit Exercise Log</h3>
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group"> 
-                <label>Username: </label>
-                <select ref="userInput"
-                    required
-                    className="form-control"
-                    value={this.state.username}
-                    onChange={this.onChangeUsername}>
-                    {
-                      this.state.users.map(function(user) {
-                        return <option 
-                          key={user}
-                          value={user}>{user}
-                          </option>;
-                      })
-                    }
-                </select>
-              </div>
-              <div className="form-group"> 
-                <label>Description: </label>
-                <input  type="text"
-                    required
-                    className="form-control"
-                    value={this.state.description}
-                    onChange={this.onChangeDescription}
-                    />
-              </div>
-              <div className="form-group">
-                <label>Duration (in minutes): </label>
-                <input 
-                    type="text" 
-                    className="form-control"
-                    value={this.state.duration}
-                    onChange={this.onChangeDuration}
-                    />
-              </div>
-              <div className="form-group">
-                <label>Date: </label>
-                <div>
-                  <DatePicker
-                    selected={this.state.date}
-                    onChange={this.onChangeDate}
-                  />
-                </div>
-              </div>
-      
-              <div className="form-group">
-                <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
-              </div>
-            </form>
+    window.location = '/';
+  }
+
+  render() {
+    return (
+    <div>
+      <h3>Edit Exercise Log</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group"> 
+          <label>Username: </label>
+          <select ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}>
+              {
+                this.state.users.map(function(user) {
+                  return <option 
+                    key={user}
+                    value={user}>{user}
+                    </option>;
+                })
+              }
+          </select>
         </div>
-        )
-    }
+        <div className="form-group"> 
+          <label>Description: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+              />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input 
+              type="text" 
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+              />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+    )
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //This is where we create our front end react app
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 
 //Do I need to delete strict mode?  
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
