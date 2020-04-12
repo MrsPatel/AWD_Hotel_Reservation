e31d7639f8a87df00096a6bdc0bbad95429c994e
sriver7 <rsonny89@gmail.com>
2020-04-12 19:16:28 -0400

    Fixing Navigation Bar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -10,6 +10,42 @@ export default class Navbar extends Component {
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create Guest</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Booking</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/rooms" className="nav-link">Rooms</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/services" className="nav-link">Services</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/attractions" className="nav-link">Attractions</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">About</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Contact</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+/*
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">AWD Hotel Services</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
           <Link to="/" className="nav-link">Exercises</Link>
           </li>
           <li className="navbar-item">
@@ -42,3 +78,4 @@ export default class Navbar extends Component {
     );
   }
 }
+*/
