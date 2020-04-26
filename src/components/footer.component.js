950774c6f2ee360a5532e68d0ff7b29c206f2661
ksoni12 <ksoni1@students.towson.edu>
2020-04-26 15:39:31 -0400

    social media links

--- src/components/footer.component.js
+++ src/components/footer.component.js
@@ -11,17 +11,17 @@ export default class Navbar extends Component {
         <footer class="w3-padding-32 w3-black w3-center w3-margin-top">
           <h5>Find Us On</h5>
           <div class="w3-xlarge w3-padding-16" >
-            <i class="fa fa-facebook-official w3-hover-opacity"></i>
+            <a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
             &emsp; 
-            <i class="fa fa-instagram w3-hover-opacity"></i>
+            <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
             &emsp; 
-            <i class="fa fa-snapchat w3-hover-opacity"></i>
+            <a href="https://www.snapchat.com/" target="_blank"><i class="fa fa-snapchat w3-hover-opacity"></i></a>
             &emsp; 
-            <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+            <a href="https://www.pinterest.com/" target="_blank"><i class="fa fa-pinterest-p w3-hover-opacity"></i></a>
             &emsp; 
-            <i class="fa fa-twitter w3-hover-opacity"></i>
+            <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
             &emsp; 
-            <i class="fa fa-linkedin w3-hover-opacity"></i>
+            <a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
           </div>
           <p>COSC 617 Project</p>
         </footer>
@@ -29,6 +29,11 @@ export default class Navbar extends Component {
       );
     }    
 }
+
+
+
+
+
 
 
 
