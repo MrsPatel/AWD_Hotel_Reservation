37d1f42982f9c5ab2b835faf8eec5e662d90ab62
sriver7 <rsonny89@gmail.com>
2020-04-26 21:14:57 -0400

    Added error page wildcard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 //Import components
@@ -8,10 +8,12 @@ import Footer from "./components/footer.component";
 import Contact from "./components/contact.component";
 import Home from "./components/home.component";
 import Rooms from "./components/rooms.component";
-//import ErrorPage from "./components/error.component.js";
+import ErrorPage from "./components/error.component.js";
 import BookingsList from "./components/bookings-list.component";
 import EditBooking from "./components/edit-booking.component"
 import CreateBooking from "./components/create-booking.component";
+//Not needed, if we need more rooms we can add with postman or
+//insomnia using JSON into the Database
 //import CreateRoom from "./components/create-room.component";
 import Services from "./components/services.component";
 import Attractions from "./components/attractions.component";
@@ -25,14 +27,18 @@ function App() {
         <Navbar />
         <br/>
         <br/>
-        <Route path="/" exact component={Home} />
-        <Route path="/BookingList" exact component={BookingsList} />
-        <Route path="/editBooking/:id" exact component={EditBooking} />
-        <Route path="/createBooking" exact component = {CreateBooking}/>
-        <Route path="/rooms" exact component={Rooms} />
-        <Route path="/services" exact component={Services} />
-        <Route path="/attractions" exact component={Attractions} />
-        <Route path="/about" exact component={About} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/BookingList" exact component={BookingsList} />
+          <Route path="/editBooking/:id" exact component={EditBooking} />
+          <Route path="/createBooking" exact component = {CreateBooking}/>
+          <Route path="/rooms" exact component={Rooms} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/attractions" exact component={Attractions} />
+          <Route path="/about" exact component={About} />
+          <Route exact component={ErrorPage}/>
+        </Switch>
+
         <Contact/>
         <Footer />
       </div>
--- src/components/error.component.js
+++ src/components/error.component.js
@@ -9,6 +9,8 @@ export default class Navbar extends Component {
           <nav className="">
             <div className="collapse" id="ERROR"></div>
             <h2>ERROR THIS PAGE DOES NOT EXIST</h2>
+            <h2>Please click one of our tabs to reach our hotel pages.</h2>
+            <img src="errorImage.jpg" alt = "ErrorImage" width="1200" height="1000"></img>
           </nav>
           
       );
