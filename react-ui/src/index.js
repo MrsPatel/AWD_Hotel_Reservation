ab2dc21d233e532e3ffc8a7e0fd0eb93e2832c43
bensonliberman28 <54550403+bensonliberman@users.noreply.github.com>
2020-04-29 20:26:36 -0400

    package.json change

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 //This is where we create our front end react app
 import App from './App';
 
-//service worker
+//import * as serviceWorker from './serviceWorker';
 
 //Loading the app into this element
 ReactDOM.render(<App />, document.getElementById('root'));
