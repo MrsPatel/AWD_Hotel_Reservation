771e64bb2c62d5acc7f893e7229d1df9540ec14a
bensonliberman28 <54550403+bensonliberman@users.noreply.github.com>
2020-04-29 21:23:10 -0400

    heroku debugging

--- react-ui/src/components/create-booking.component.js
+++ react-ui/src/components/create-booking.component.js
@@ -48,7 +48,7 @@ export default class CreateBooking extends Component {
     it loads, it's going to run this
     */
     componentDidMount(){
-        axios.get('http://localhost:5000/room/')
+        axios.get('http://localhost:5000.com/room/')
             .then(response => {
                 if (response.data.length > 0){
                     this.setState({
